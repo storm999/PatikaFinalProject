d9f4aa4e7cbbda74259ae3957ce0377aa6186411
storm999 <storm_999_@hotmail.com>
2023-02-14 17:45:17 +0300

    TestCode

--- xUnitTests/MovieTests.cs
+++ xUnitTests/MovieTests.cs
@@ -12,25 +12,6 @@ using Xunit.Abstractions;
 
 namespace xUnitTests
 {
-    public class TestsBase  
-    {
-        protected MyDbContext _dbContext;
-        protected IMapper mapper;
-
-        public TestsBase( )
-        {
-            _dbContext = new MyDbContext(new DbContextOptionsBuilder<MyDbContext>().UseSqlServer("Data Source=Dell; Initial Catalog=patikaFinalProject; Integrated Security=true; TrustServerCertificate=True;").Options );
-            MapperConfiguration configuration = new MapperConfiguration(opt => {
-                                                                                    opt.AddProfile(new CustomerProfile());
-                                                                                    opt.AddProfile(new ActorProfile());
-                                                                                    opt.AddProfile(new DirectorProfile());
-                                                                                    opt.AddProfile(new MovieProfile());
-                                                                                    opt.AddProfile(new OrderMovieProfile());
-                                                                                });
-            mapper = configuration.CreateMapper();
-        }
-    }
-
     public class MovieTests : TestsBase
     {
         private MovieService movieService;
@@ -40,35 +21,59 @@ namespace xUnitTests
             movieService = new MovieService(_dbContext, mapper, new MovieCreateDTOValidator(), new MovieDTOValidator());
         }
         
-        public static IEnumerable<object[]> SplitCountData =>
-                                                 new List<object[]>
-                                                 {
-                                                        new object[] { "a" , 1, new DateTime(2020,01,01) },
-                                                        new object[] { "aaa" , -1, new DateTime(2020,01,01) },
-                                                        new object[] { "aaa", 2, new DateTime(2025,01,01) },
-                                                        new object[] { "aaa", 1, new DateTime(1799,01,01) }
-                                                 };
+        public static IEnumerable<object[]> InvalidInputList =>  new List<object[]>
+                                                                 {
+                                                                        new object[] { "a" , 1, new DateTime(2020,01,01) },
+                                                                        new object[] { "aaa" , -1, new DateTime(2020,01,01) },
+                                                                        new object[] { "aaa", 2, new DateTime(2025,01,01) },
+                                                                        new object[] { "aaa", 1, new DateTime(1799,01,01) }
+                                                                 };
 
-        [Theory, MemberData(nameof(SplitCountData))]
-        public void Test1(string name, int price, DateTime date)
+        [Theory, MemberData(nameof(InvalidInputList))]
+        public void MovieInvalidInputTests(string name, int price, DateTime date)
         {
             MovieCreateDTO dto = new MovieCreateDTO();
-            
+
             dto.MovieName = name;
             dto.Price = price;
             dto.MovieYear = date;
-            
-            
+
+
             /*Func<Task> act = async () => await movieService.Create(dto);
-            act.Should().NotThrowAsync<InvalidOperationException>();*/
-            
+            act.Should().ThrowAsync<InvalidOperationException>();*/
+
             //FluentActions.Invoking(() => movieService.Create(dto)).Should().ThrowAsync<ArgumentNullException>().Result.And.Message;
-            
-            MovieCreateDTOValidator mcv= new MovieCreateDTOValidator();
-            ValidationResult valRes =  mcv.Validate(dto);
+
+            MovieCreateDTOValidator mcv = new MovieCreateDTOValidator();
+            ValidationResult valRes = mcv.Validate(dto);
             valRes.Errors.Count.Should().BeGreaterThan(0);
             //valRes.Errors.Should().Contain(new ValidationFailure("Price", "'Price' must be greater than or equal to '0'."));
 
+        }
+
+
+
+
+        public static IEnumerable<object[]> ValidInputList => new List<object[]>
+                                                                 {
+                                                                        new object[] { "aaa" , 0, new DateTime(2020,01,01) },
+                                                                        new object[] { "00", 1, DateTime.Now },
+                                                                        new object[] { "aa", 1, new DateTime(1800,01,01) }
+                                                                 };
+
+        [Theory, MemberData(nameof(ValidInputList))]
+        public void MovieValidInputTests(string name, int price, DateTime date)
+        {
+            MovieCreateDTO dto = new MovieCreateDTO();
+
+            dto.MovieName = name;
+            dto.Price = price;
+            dto.MovieYear = date;
+
+
+            MovieCreateDTOValidator mcv = new MovieCreateDTOValidator();
+            ValidationResult valRes = mcv.Validate(dto);
+            valRes.Errors.Count.Should().BeLessThanOrEqualTo(0);
         }
     }
 }
